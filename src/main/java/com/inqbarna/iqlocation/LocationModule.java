c6f445123ac432ffd81f3957a7f09a36eeb63f89
David García <david.garcia@inqbarna.com>
2016-12-01 07:59:47 +0100

    Add Basic location Dagger Module

---
+++ src/main/java/com/inqbarna/iqlocation/LocationModule.java
@@ -0,0 +1,46 @@
+package com.inqbarna.iqlocation;
+
+import android.content.Context;
+
+import com.google.android.gms.location.LocationRequest;
+import com.inqbarna.iqlocation.annotation.BatteryConservativeLocation;
+import com.inqbarna.iqlocation.annotation.FastLocation;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+
+/**
+ * @author David García <david.garcia@inqbarna.com>
+ * @version 1.0 30/11/16
+ */
+@Module
+public class LocationModule {
+
+    private static final long QUICK_FASTEST_INTERVAL = 5 * 1000; // 5 seconds in millis
+    private Context mContext;
+
+    public LocationModule(Context context) {
+        mContext = context.getApplicationContext();
+    }
+
+    @Singleton
+    @Provides
+    @FastLocation
+    LocationHelper provideFastLocation() {
+        return LocationHelper.builder(mContext)
+                             .setFastestInterval(QUICK_FASTEST_INTERVAL)
+                             .setInterval(QUICK_FASTEST_INTERVAL)
+                             .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                             .build();
+    }
+
+    @Singleton
+    @Provides
+    @BatteryConservativeLocation
+    LocationHelper provideBatterySaverLocation() {
+        return LocationHelper.builder(mContext).build();
+    }
+
+}
